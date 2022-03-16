a213949ff2ee996ee4b0ab316f1b744b62db0f7e
Jakub Jaworski <sztos@serialkiller.home>
2022-03-17 00:04:30 +0100

    added classes and some redesigns to look better :)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import Creatures from './creatures/Creatures.js'
 import Creature from './creature/Creature.js'
 import { useState } from 'react';
@@ -9,26 +9,29 @@ function App() {
 
   const showList = () => {
     setShowCreatures(!showCreatures);
+    showCreatureSearch && setShowCreatureSearch(!showCreatureSearch);
   }
 
   const showForm = () => {
     setShowCreatureSearch(!showCreatureSearch);
+    showCreatures && setShowCreatures(!showCreatures);
   }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <div>
+      <main className="App-main">
+        <h1>Pick what you want to see:</h1>
+        <div className='holder'>
           <h2 className='clickToShow' onClick={showList}>
             Complete list of creatures
           </h2>
           {showCreatures && <Creatures />}
         </div>
-        <div>
-          <h2 onClick={showForm}>Search for creature</h2>
+        <div className='holder'>
+          <h2 className='clickToShow' onClick={showForm}>Search for creature</h2>
           {showCreatureSearch && <Creature />}
         </div>
-      </header>
+      </main>
     </div>
   );
 }
