ec78640671c18e04cb7aa3bb56ed5611cb4a5f7c
Jakub Jaworski <sztos@serialkiller.home>
2022-03-16 19:01:10 +0100

    created show and hide elements and renders

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import './App.css';
 import Creatures from './creatures/Creatures.js'
+import Creature from './creature/Creature.js'
 import { useState } from 'react';
 
 function App() {
-  const [showCreatures, setShowCreatures] = useState(true);
+  const [showCreatures, setShowCreatures] = useState(false);
+  const [showCreatureSearch, setShowCreatureSearch] = useState(false);
 
-  const showCreaturesHandle = () => {
+  const showList = () => {
     setShowCreatures(!showCreatures);
+  }
+
+  const showForm = () => {
+    setShowCreatureSearch(!showCreatureSearch);
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h2 onClick={showCreaturesHandle}>Complete list of creatures.</h2>
-        {showCreatures && <Creatures />}
+        <div>
+          <h2 className='clickToShow'
+            onClick={showList}>
+            Complete list of creatures
+            {showCreatures && <Creatures />}
+          </h2>
+        </div>
+        <div>
+          <h2 onClick={showForm}>Search for creature</h2>
+          {showCreatureSearch && <Creature />}
+        </div>
       </header>
     </div>
   );
