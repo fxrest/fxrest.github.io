66f6cc6f90f2ca72e7494feb03deaf4e4399c092
Jakub Jaworski <sztos@serialkiller.home>
2022-03-16 17:53:41 +0100

    added working list of creatures

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Creatures from './creatures/Creatures.js'
+import { useState } from 'react';
 
 function App() {
+  const [showCreatures, setShowCreatures] = useState(true);
+
+  const showCreaturesHandle = () => {
+    setShowCreatures(!showCreatures);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2 onClick={showCreaturesHandle}>Complete list of creatures.</h2>
+        {showCreatures && <Creatures />}
       </header>
     </div>
   );
---
+++ src/creatures/Creatures.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import './creatures.scss';
+
+export default function Creatures() {
+    const [creatures, setCreatures] = useState([])
+
+    useEffect(() => {
+        const get = async () => {
+            const r = await fetch("https://api.tibiadata.com/v3/creatures");
+            const d = await r.json();
+            console.log(d)
+            setCreatures(d.creatures.creature_list)
+        }
+        get()
+    }, [])
+
+    return (
+        <div>
+            {creatures.map((cre, index) => (
+                <div className='creatureListHolder' key={index}>
+                    {cre.name}
+                    <img src={cre.image_url} alt="" />
+                </div>
+            ))}
+        </div>
+    )
+}
