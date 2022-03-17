f1161e0d2ed25c36c61e05893a45ff845ee1b47a
Jakub Jaworski <sztos@serialkiller.home>
2022-03-17 13:01:14 +0100

    added styled footer and navbar

---
+++ src/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.scss'
+
+export default function Footer() {
+    return (
+        <footer className='footerMain'>
+            <strong>
+                <span>TibiaBestiary</span>
+            </strong> created by <strong><a href='https://github.com/fxrest'>fxrest</a></strong></footer>
+    )
+}
---
+++ src/navbar/Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Navbar.scss'
+
+export default function Navbar() {
+    return (
+        <nav className='navMain'><strong><span>Tibia</span></strong>Bestiary</nav>
+    )
+}
